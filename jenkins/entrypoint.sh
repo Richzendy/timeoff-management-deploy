583681a3b9aa28848d9f7ba5e2f666ec22785cf2
Richzendy <richzendy@gmail.com>
2022-04-21 17:06:56 +0000

    Improvements

--- jenkins/entrypoint.sh
+++ jenkins/entrypoint.sh
@@ -1,5 +1,6 @@
 #! /bin/bash -e
 
+#whoami 
 #mkdir -p /var/jenkins_home/.ssh
 #mv /usr/share/jenkins/ref/.ssh/id_rsa /var/jenkins_home/.ssh
 #chmod 600 /var/jenkins_home/.ssh/id_rsa
@@ -7,6 +8,6 @@
 echo "start JENKINS"
 # if 'docker run' first argument start with '--' the user is passing jenkins launcher arguments
 if [[ $# -lt 1 ]] || [[ "$1" == "--"* ]]; then
-    exec /bin/tini -- /usr/local/bin/jenkins.sh "$@"
+    exec /sbin/tini -- /usr/local/bin/jenkins.sh "$@"
 fi
 exec "$@"
